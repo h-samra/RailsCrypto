d3ba90cb7d6323a68b0c4efdf5d5b9f43d3ebcf7
Herman Samra <herman.samra1@gmail.com>
2018-06-15 07:14:02 +0000

    Added Coin Market Cap API & lookup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,38 @@ class HomeController < ApplicationController
   def index
     require 'net/http'
     require 'json'
-    
+
     @url = 'https://api.coinmarketcap.com/v1/ticker/'
     @uri = URI(@url)
+    #JSON formatting 
     @response = Net::HTTP.get(@uri)
+    #ruby hash formatting
     @coins = JSON.parse(@response)
+
   end
   
   def about
   end
+  
+  def lookup
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    #JSON formatting 
+    @response = Net::HTTP.get(@uri)
+    #ruby hash formatting
+    @lookup_coin = JSON.parse(@response)
+    
+    @symbol = params[:sym]
+    
+    if @symbol
+      @symbol = @symbol.upcase
+    end
+    
+    
+  end
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'home/about'
+  get 'home/lookup'
+  post '/home/lookup' => 'home/lookup'
   root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
